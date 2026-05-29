b5ad3c7720f286f4f9c32331f89704981e539f49
User <user@Users-MacBook-Air.local>
2026-05-29 02:01:33 -0300

    test: mover trofeo hero landing

--- src/components/landing/landing-page.tsx
+++ src/components/landing/landing-page.tsx
@@ -94,7 +94,7 @@ export function LandingPage() {
               className="hidden min-[375px]:block"
               style={{
                 position: "absolute",
-                right: "-50px",
+                right: "-60px",
                 top: "47%",
                 transform: "translateY(-50%)",
                 height: "200px",
